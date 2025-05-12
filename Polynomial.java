557568ba18f4a0c62dddc869398e31b98e74cd42
Travise <travise42@icloud.com>
2025-05-12 14:44:23 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,40 @@
+public class Polynomial {
+    private double[] coefficients;
+
+    public Polynomial() {
+        coefficients = new double[]{0};
+    }
+
+    public Polynomial(double[] coefficients) {
+        this.coefficients = coefficients;
+    }
+
+    public Polynomial add(Polynomial poly) {
+        final int SIZE = Math.max(this.coefficients.length, poly.coefficients.length);
+        double[] new_coefficients = new double[SIZE];
+        for (int i = 0; i < SIZE; i++)
+            new_coefficients[i] = this.get_coefficient(i) + poly.get_coefficient(i);
+        return new Polynomial(new_coefficients);
+    }
+
+    public double evaluate(double x) {
+        double sum = 0;
+        for (int power = 0; power < coefficients.length; power++)
+            sum += coefficients[power] * Math.pow(x, power);
+        return sum;
+    }
+
+    boolean hasRoot(double x) {
+        return evaluate(x) == 0;
+    }
+
+    /**
+     * Return the ith coefficient of the polynomial.
+     * Returns 0 if i < 0.
+     */
+    public double get_coefficient(int i) {
+        if (0 <= i && i < coefficients.length)
+            return coefficients[i];
+        return 0;
+    }
+}
