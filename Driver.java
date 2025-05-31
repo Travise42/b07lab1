a4650f2b3c3b247031d79d36a488817aab0cc2c2
Travise <travise42@icloud.com>
2025-05-30 22:31:19 -0400

    modified Polynomial.ava and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,19 +1,43 @@
+import java.io.File;
+
 public class Driver {
     public static void main(String [] args) {
         
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
 
-        double[] c1 = {6, 0, 0, 5};
-        Polynomial p1 = new Polynomial(c1);
-        double[] c2 = {0, -2, 0, 0, -9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
+        double[] c1a = {6, 5};
+        int[] c1b = {0, 3};
+        Polynomial p1 = new Polynomial(c1a, c1b);
+        double[] c2a = {-2, -9};
+        int[] c2b = {1, 4};
+        Polynomial p2 = new Polynomial(c2a, c2b);
+        System.out.println("p1 = " + p1);
+        System.out.println("p2 = " + p2);
+
+        Polynomial s1 = p1.add(p2);
+        System.out.println("s1 = " + s1);
+        System.out.println("s1(0.1) = " + s1.evaluate(0.1));
+
+        Polynomial s2 = p1.multiply(p2);
+        System.out.println("s2 = " + s2);
+        System.out.println("s2(0.1) = " + s2.evaluate(0.1));
         
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
+        if(s1.hasRoot(1))
+            System.out.println("1 is a root of s1");
         else
-            System.out.println("1 is not a root of s");
+            System.out.println("1 is not a root of s1");
+        
+        if(s2.hasRoot(0))
+            System.out.println("0 is a root of s2");
+        else
+            System.out.println("0 is not a root of s2");
+
+        File f = new File("poly_file.txt");
+        Polynomial p3 = new Polynomial(f);
+        System.out.println(p3);
+        System.out.println("p3(0.1) = " + p3.evaluate(0.1));
+
+        s2.saveToFile("poly_file2.txt");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,30 +1,129 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Polynomial {
     public double[] coefficients;
+    public int[] exponents;
 
     public Polynomial() {
         coefficients = new double[]{0};
+        exponents = new int[]{0};
     }
 
-    public Polynomial(double[] coefficients) {
-        if (coefficients == null || coefficients.length == 0) {
-            this.coefficients = new double[]{0};
+    public Polynomial(double[] coefficients, int[] exponents) {
+        // Handle null parameters
+        if (coefficients == null || exponents == null) {
+            this.coefficients = new double[0];
+            this.exponents = new int[0];
             return;
         }
-        this.coefficients = coefficients.clone();
+
+        // Copy over values
+        this.coefficients = coefficients;
+        this.exponents = exponents;
+        
+        // Remove terms with 0 as their coefficient
+        filter();
+    }
+
+    public Polynomial(File file) {
+        try {
+            // Retrieve polynomial from file
+            Scanner s = new Scanner(file);
+            String poly = s.next();
+            s.close();
+
+            // Seperate polynomial into terms
+            poly = poly.replace("-", "+-");
+            String[] terms = poly.split("\\+");
+
+            // Initialize arrays for values
+            coefficients = new double[terms.length];
+            exponents = new int[terms.length];
+
+            // Decode each term
+            for (int i = 0; i < terms.length; i++) {
+
+                // Seperate coefficients and exponents
+                String[] elements = terms[i].split("x");
+                if (elements.length == 0) continue;
+
+                // Store coefficients and exponents
+                coefficients[i] = Double.parseDouble(elements[0]);
+                if (elements.length > 1) exponents[i] = Integer.parseInt(elements[1]);
+                else exponents[i] = 0;
+            }
+
+            // Remove terms with 0 as their coefficient
+            filter();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File does not exist. Could not initialize Polynomial.");
+        }
     }
 
     public Polynomial add(Polynomial poly) {
-        final int SIZE = Math.max(this.coefficients.length, poly.coefficients.length);
-        double[] new_coefficients = new double[SIZE];
-        for (int i = 0; i < SIZE; i++)
-            new_coefficients[i] = this.get_coefficient(i) + poly.get_coefficient(i);
-        return new Polynomial(new_coefficients);
+
+        // Get length of new polynomial exponents array using pointers incrementing in accending order to handle duplicates
+        int length = 0, this_iterator = 0, poly_iterator = 0;
+        for (int temp_iterator = this_iterator; this_iterator < this.exponents.length && poly_iterator < poly.exponents.length; length++) {
+            if (this.exponents[temp_iterator] <= poly.exponents[poly_iterator]) this_iterator++;
+            if (this.exponents[temp_iterator] >= poly.exponents[poly_iterator]) poly_iterator++;
+        }
+        length += this.exponents.length + poly.exponents.length - this_iterator - poly_iterator;
+
+        // Initialize arrays to store values
+        double[] new_coefficients = new double[length];
+        int[] new_exponents = new int[length];
+        
+        // Initialize/ reset pointers
+        this_iterator = 0;
+        poly_iterator = 0;
+        int index = 0;
+
+        // Store values into arrays using pointers
+        while (this_iterator < this.exponents.length || poly_iterator < poly.exponents.length) {
+
+            // If smaller, store this value and move this pointer
+            int temp_iterator = this_iterator;
+            if (poly_iterator >= poly.exponents.length || temp_iterator < this.exponents.length && this.exponents[temp_iterator] <= poly.exponents[poly_iterator]) {
+                new_coefficients[index] += this.coefficients[temp_iterator];
+                new_exponents[index] = this.exponents[temp_iterator];
+                this_iterator++;
+            }
+
+            // If smaller, store poly value and move poly pointer
+            if (temp_iterator >= this.exponents.length || poly_iterator < poly.exponents.length && this.exponents[temp_iterator] >= poly.exponents[poly_iterator]) {
+                new_coefficients[index] += poly.coefficients[poly_iterator];
+                new_exponents[index] = poly.exponents[poly_iterator];
+                poly_iterator++;
+            }
+
+            index++;
+        }
+
+        return new Polynomial(new_coefficients, new_exponents);
+    }
+
+    public Polynomial multiply(Polynomial poly) {
+        
+        // Expand out each of the terms and add to new_poly
+        Polynomial new_poly = new Polynomial();
+        for (int i = 0; i < poly.exponents.length; i++) new_poly = new_poly.add(multByTerm(poly.coefficients[i], poly.exponents[i]));
+        
+        // Remove terms that cancelled out (coefficients of 0)
+        new_poly.filter();
+        return new_poly;
     }
 
     public double evaluate(double x) {
+        
+        // Equate the values by multiplying coefficients by x raised to the exponents
         double sum = 0;
-        for (int power = 0; power < coefficients.length; power++)
-            sum += coefficients[power] * Math.pow(x, power);
+        for (int i = 0; i < exponents.length; i++) sum += coefficients[i] * Math.pow(x, exponents[i]);
         return sum;
     }
 
@@ -32,13 +131,78 @@ public class Polynomial {
         return evaluate(x) == 0;
     }
 
-    /**
-     * Return the ith coefficient of the polynomial.
-     * Returns 0 if i < 0.
-     */
-    public double get_coefficient(int i) {
-        if (0 <= i && i < coefficients.length)
-            return coefficients[i];
-        return 0;
+    @Override
+    public String toString() {
+        
+        // No exponents means the polynomial is 0
+        if (exponents.length == 0) return "0";
+
+        // Create polynomial string by iterating through each term
+        String string = "";
+        for (int i = 0; i < exponents.length; i++) {
+
+            // Seperate non-negative terms by '+'
+            if (i > 0 && coefficients[i] > 0) string += "+";
+
+            // Append the term to string, only including x if the exponent is not 0 and only including the exponent if its 2 or more
+            string += String.valueOf(coefficients[i]);
+            if (exponents[i] > 0) string += "x";
+            if (exponents[i] > 1) string += String.valueOf(exponents[i]);
+        }
+
+        return string;
+    }
+
+    public void saveToFile(String file) {
+        try {
+            FileWriter output = new FileWriter(file, false);
+
+            // Save polynomial string to file
+            output.write(this.toString());
+
+            output.close();
+        } catch (IOException e) {
+            System.out.println("File does not exists. Could not save Polynomial.");
+        }
+
+    }
+
+    private Polynomial multByTerm(double coefficient, int exponent) {
+
+        // Create copy to by multiplied and returned
+        Polynomial new_poly = new Polynomial(this.coefficients, this.exponents);
+
+        // Multiply each term by the given coefficient and exponent
+        for (int i = 0; i < new_poly.exponents.length; i++) {
+            new_poly.coefficients[i] *= coefficient;
+            new_poly.exponents[i] += exponent;
+        }
+
+        return new_poly;
+    }
+
+    private void filter() {
+
+        // Get length of new polynomial arrays
+        int length = 0;
+        for (int i = 0; i < coefficients.length; i++) if (coefficients[i] != 0) length++;
+        
+        // Initialize arrays to store values
+        double[] new_coefficients = new double[length];
+        int[] new_exponents = new int[length];
+
+        // Iterate through coefficients and skip coefficients of 0 as they are redundent
+        for (int i = 0, index = 0; i < coefficients.length; i++) {
+            if (coefficients[i] == 0) continue;
+
+            // Copy over non-zero values
+            new_coefficients[index] = coefficients[i];
+            new_exponents[index] = exponents[i];
+            index++;
+        }
+
+        // Store zero-free values
+        coefficients = new_coefficients;
+        exponents = new_exponents;
     }
 }
